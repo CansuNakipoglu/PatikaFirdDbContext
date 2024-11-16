e06c6bae16fc65f65d3c02a3688227b40822ee70
CANSU NAKİPOĞLU ESERGÜN <119347726+CansuNakipoglu@users.noreply.github.com>
2024-11-17 00:38:59 +0300

    Initial Commit

---
+++ Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstBasic.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Platform { get; set; }
+
+        public decimal Rating { get; set; }
+        
+    }
+}
---
+++ PatikaFirstDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeFirstBasic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstBasic
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public PatikaFirstDbContext(DbContextOptions options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
